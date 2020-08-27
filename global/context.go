eea8301cea1afa233c625580642b55c00dc951e9
J. Behrmann <jjxxs@users.noreply.github.com>
2020-08-27 11:42:41 +0200

    Added shutdown-group context (#8)
    * Using reference for RWMutex
    
    * Added event-bus
    
    * Added shutdown-context

---
+++ global/context.go
@@ -0,0 +1,55 @@
+package global
+
+import (
+	"context"
+	"errors"
+	"sync"
+)
+
+const shutdownGroupKey = "shutdownGroup"
+
+// Provides a context with a cancel-func that carries a WaitGroup.
+// Callbacks can be registered to this context and will be called
+// when the context is cancelled.
+func GetShutdownContext(ctx context.Context) (context.Context, func()) {
+	ctx, cancel := context.WithCancel(ctx)
+	if val := ctx.Value(shutdownGroupKey); val == nil {
+		ctx = context.WithValue(ctx, shutdownGroupKey, &sync.WaitGroup{})
+	}
+	return ctx, cancel
+}
+
+// Blocks until all callbacks were called in the WaitGroup carried
+// by the given context.
+func WaitForShutdownContext(ctx context.Context) error {
+	if val := ctx.Value(shutdownGroupKey); val != nil {
+		if shutdownGroup, ok := val.(*sync.WaitGroup); ok {
+			shutdownGroup.Wait()
+		} else {
+			return errors.New("value has invalid type")
+		}
+	} else {
+		return errors.New("value not found")
+	}
+	return nil
+}
+
+// When the given context is cancelled through its cancel-func, the given
+// callback will be called.
+func RegisterShutdownCallback(ctx context.Context, callback func()) error {
+	if val := ctx.Value(shutdownGroupKey); val != nil {
+		if shutdownGroup, ok := val.(*sync.WaitGroup); ok {
+			shutdownGroup.Add(1)
+			go func() {
+				<-ctx.Done()
+				callback()
+				shutdownGroup.Done()
+			}()
+		} else {
+			return errors.New("value has invalid type")
+		}
+	} else {
+		return errors.New("value not found")
+	}
+	return nil
+}
---
+++ global/context_test.go
@@ -0,0 +1,38 @@
+package global
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestShutdownContext(t *testing.T) {
+	ctx, cancel := GetShutdownContext(context.Background())
+	wg := &sync.WaitGroup{}
+
+	for i := 0; i < 10; i++ {
+		if err := RegisterShutdownCallback(ctx, func() { wg.Done() }); err != nil {
+			t.Fatal(err)
+		}
+		wg.Add(1)
+	}
+
+	var shutdown time.Time
+	go func() {
+		if err := WaitForShutdownContext(ctx); err != nil {
+			t.Fatal(err)
+		}
+		shutdown = time.Now()
+	}()
+
+	time.Sleep(1 * time.Second)
+	cancel()
+	wg.Wait()
+	diff := time.Since(shutdown)
+	t.Logf("diff was %d milliseconds %d microseconds %d nanoseconds",
+		diff.Milliseconds(), diff.Microseconds(), diff.Nanoseconds())
+	if diff.Milliseconds() > 100 {
+		t.Fatal("diff was high, does blocking work for shutdown-context?")
+	}
+}
